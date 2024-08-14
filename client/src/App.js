9bae7ee4870918f3727f657e46a54c4fdd84d76f
Jaydeep Jadhav <jaydeepjadhav784@gmail.com>
2024-08-14 18:30:23 +0530

    initial commit with client and server

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <div className="app">
+        
         <BrowserRouter>
         <ThemeProvider theme={theme}>
         <CssBaseline />
--- server/index.js
+++ server/index.js
@@ -13,22 +13,23 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
-import { verifyToken } from "./middleware/auth.js";
+
 
 /* configuration */
-
-app.use(
-  cors({
-    origin: ["https://deploy-mern-lwhq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+
+app.use(
+  cors({
+    origin: ["https://deploy-mern-lwhq.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
